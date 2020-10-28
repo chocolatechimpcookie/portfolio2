aea973c9f9862138ea292622d1275ca906de4317
Cina <memaralicina@gmail.com>
2020-10-28 19:35:42 -0400

    Added working UI router

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 angular.module('portfolio2App', ['ui.router'])
-.config(['@stateProvider'])
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider)
+{
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+    .state(
+    'home',
+    {
+        url:'/',
+        templateUrl:'views/welcome-section.html'
+    })
+
+    .state('school',
+    {
+        url:'/school',
+        templateUrl:'views/school.html'
+    })
+    ;
+}]);
